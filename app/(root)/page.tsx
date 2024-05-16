e15929c9c926bff07499b787a8132a833ab431c5
hananbabar <60232569+hananbabar98@users.noreply.github.com>
2024-05-16 08:07:50 +0500

    feat: BA-002 - Dashboard Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
+import { TotalBalanceBox } from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Hanan" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account and transactions efficiently"
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
